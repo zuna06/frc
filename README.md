28c3fa8c85c5e1875cc4082d9419be1fb44ec673
zuna06 <tanishambala94@gmail.com>
2023-10-09 16:58:34 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# frc
+# FRC
+
+#### Programming Repository For FRC Team 9025 
+
+#### The lessons under ./lessons only go over Java topics needed for FRC only
+#### Does not include: user input, string manipulation, file handling, and advanced
+#### Java OOP topics that are beyond the scope of FRC Programming
+
---
+++ lessons/Intro.java
@@ -0,0 +1,32 @@
+public class Intro {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+
+        //Variables
+
+        int int_num = 54;
+        char character = 'h';
+        String char_arr = "Hello World";
+
+        //What we will be using
+
+        int num2 = 54;
+        float num3 = 54.23f;
+        double num4 = 54.234435353;
+        boolean bool = true;
+
+        //Naming Conventions
+        //Make your variable descriptive 
+
+        int metersPerSecond;
+        //vs
+        int mps;
+
+        //Operaters
+
+
+
+
+    }
+}
